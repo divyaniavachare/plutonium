897d709cf4a9ad10f61ff3e072b4931b4736580f
divyaniavachare <divyaniavachare122@gmail.com>
2022-08-10 13:12:24 +0530

    updated file

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,20 +2,6 @@ const express = require("express");
 const abc = require("../introduction/intro");
 const router = express.Router();
 
-//router.get('/test-me', function (req, res) {
-//  console.log('My batch is', abc.name)
-// abc.printName()
-//logger.welcome()
-
-//res.send('My second ever api!')
-//});
-
-//router.get('/students', function (req, res){
-//  let students = ['Sabiha', 'Neha', 'Akash']
-//res.send(students)
-//})
-
-//Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
 //problem:01
 router.get("/movies", function (req, res) {
   let movies = ["don", "uri", "sairat", "tani"];
@@ -27,18 +13,13 @@ router.get("/movies", function (req, res) {
 router.get("/movies/:indexNumber", function (req, res) {
   const movies = ["don", "uri", "sairat", "tani"];
   let indexNo = req.params.indexNumber;
-
+  //problem:03
   if (indexNo < 0 || indexNo >= movies.length) {
     return res.send("Invalid Index No");
   }
   let moviesName = movies[indexNo];
   res.send(moviesName);
 });
-
-// router.get("/movies", function (req, res) {});
-// router.get("/test-me", function (req, res) {
-//   res.send("this is second routes implementation");
-// });
 
 //problem:04
 
@@ -63,7 +44,7 @@ router.get("/films", function (req, res) {
   ];
   res.send(films);
 });
-
+//problem :05
 router.get("/films/:filmId", function (req, res) {
   const films = [
     {
@@ -98,6 +79,43 @@ router.get("/films/:filmId", function (req, res) {
   res.send("dummy");
 });
 
-//res.send("hello there")
+//problem:01
+//-write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+router.get("/sol1", function (req, res) {
+  //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+  let arr = [1, 2, 3, 5, 6, 7];
+
+  let total = 0;
+  for (var i in arr) {
+    total = total + arr[i];
+  }
+
+  let lastDigit = arr.pop();
+  let consecutiveSum = (lastDigit * (lastDigit + 1)) / 2;
+  let missingNumber = consecutiveSum - total;
+
+  res.send({ data: missingNumber });
+});
+
+//problem 02
+
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+router.get("/sol2", function (req, res) {
+  // logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+  let arr = [33, 34, 35, 37, 38];
+  let len = arr.length;
+
+  let total = 0;
+  for (var i in arr) {
+    total += arr[i];
+  }
+
+  let firstDigit = arr[0];
+  let lastDigit = arr.pop();
+  let consecutiveSum = ((len + 1) * (firstDigit + lastDigit)) / 2;
+  let missingNumber = consecutiveSum - total;
+
+  res.send({ data: missingNumber });
+});
 
 module.exports = router;

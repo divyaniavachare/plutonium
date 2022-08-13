064cfa5fe9e55b08cb20fb39536788ee14969f16
divyaniavachare <divyaniavachare122@gmail.com>
2022-08-14 00:00:37 +0530

    updates

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const UserModel = require("../models/userModel.js");
+const UserController = require("../controllers/userController");
+
+const data = require("../bookingFunction/bookingFunction");
+router.post("/usersBookData", data.booksName);
+router.get("/getAllBooksData", data.allBooksName);
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+  res.send("My first ever api!");
+});
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createUser", UserController.createUser);
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getUsersData", UserController.getUsersData);
 
 module.exports = router;

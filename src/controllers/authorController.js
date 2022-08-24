8a07b424bef96e07a2d00c3b49626abeb37c6104
divyaniavachare <divyaniavachare122@gmail.com>
2022-08-24 21:57:53 +0530

    assignment

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,8 +1,8 @@
-const AuthorModel= require("../models/authorModel")
+const AuthorModel= require("../models/newAuthor")
 
 const createAuthor= async function (req, res) {
-    let author = req.body
-    let authorCreated = await AuthorModel.create(author)
+    let newAuthor = req.body
+    let authorCreated = await AuthorModel.create(newAuthor)
     res.send({data: authorCreated})
 }
 
@@ -10,6 +10,15 @@ const getAuthorsData= async function (req, res) {
     let authors = await AuthorModel.find()
     res.send({data: authors})
 }
+ 
+const createPublisher=async function(req,res){
+    let newPublisher= req.body
+    let publisherCreated=await publisherModel.create(newPublisher)
+    res.send({data:publisherCreated})
+}
+
+
+
 
 module.exports.createAuthor= createAuthor
 module.exports.getAuthorsData= getAuthorsData

ec47b2441b85c904f3ed5b0d3d8996a3d126b299
divyaniavachare <divyaniavachare122@gmail.com>
2022-08-24 21:57:12 +0530

    assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,38 @@
-const authorModel = require("../models/authorModel")
-const bookModel= require("../models/bookModel")
+const bookModel = require("../models/bookModel")
+const authorModel = require("../models/newAuthor")
+const publisherModel = require("../models/newPublisher")
 
-const createBook= async function (req, res) {
-    let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
-}
 
-const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
-    res.send({data: books})
+const createBook = async function (req, res) {
+    let bk = req.body;
+    if (!bk.author) { return res.send("AuthorId detail is not found") };
+    if (!bk.publisher) { return res.send("PublisherId detail is not found") };
+    let checkIdOfAuthor = await authorModel.findById(bk.author)
+    let checkIdOfpublisher = await publisherModel.findById(bk.publisher)
+    if (!checkIdOfAuthor) { return res.send("Author is not present.") };
+    if (!checkIdOfpublisher) { return res.send("Publisher is not present.") };
+    let bookCreated = await bookModel.create(bk);
+    res.send({ data: bookCreated });
+};
+
+
+
+const allDetails = async function (req, res) {
+    let specificBook = await bookModel.find()
+
+    res.send({ data: specificBook })
 }
 
 const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
+    let specificBook = await bookModel.find().populate(["author", "publisher"])
+    res.send({ data: specificBook })
 
 }
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+
+
+module.exports.createBook = createBook
+module.exports.allDetails = allDetails
 module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+// module.exports.updateNewBook = updateNewBook
+// module.exports.updatePriceOfBook = updatePriceOfBook
